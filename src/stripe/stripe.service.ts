a4f6fd3769bc2a16ae96b9c61d44876986f3a186
s4v!0r <egbp25@outlook.com>
2026-04-01 22:24:08 -0500

    feat: implement StripeService with constructor and configuration injection

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { type ConfigType } from '@nestjs/config';
+import Stripe from 'stripe';
+
+import { stripeConfig } from './config';
 
 @Injectable()
-export class StripeService {}
+export class StripeService {
+  private stripe: Stripe;
+
+  constructor(
+    @Inject(stripeConfig.KEY)
+    stripeConfigValues: ConfigType<typeof stripeConfig>,
+  ) {
+    this.stripe = new Stripe(stripeConfigValues.stripeSecretKey);
+  }
+}
